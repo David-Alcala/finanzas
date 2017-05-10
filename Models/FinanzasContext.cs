ab90defc149e0d52b693c809b6f387a69003a00a
xtuxur <alex.delzar@gmail.com>
2017-05-09 19:33:31 -0500

    Agregar archivos de proyecto.

---
+++ Models/FinanzasContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace finanzas.Models
+{
+    public class FinanzasContext:DbContext
+    {
+        public FinanzasContext(DbContextOptions<FinanzasContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace finanzas.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        [Display(Name = "Usuario")]
+        public string UserName { get; set; }
+        [Display(Name = "Password")]
+        public string Hash { get; set; }
+        [Display(Name = "Efectivo")]
+        public float Cash { get; set; }
+    }
+}
